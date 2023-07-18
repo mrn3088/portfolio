abc4aae8488cebc589bb1ed34b600d2992247f47
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-07-18 19:58:58 +0800

    nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import { BrowserRouter } from "react-router-dom";
 
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 const App = () => {
   return (
-    <div>
-      3D Developer Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-no-repeat bg-center bg-hero-pattern bg-cover">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
