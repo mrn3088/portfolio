b8d87a4803bd747dbde69ed65ed8e86e090cc1a2
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-07-18 21:31:01 +0800

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return <p>{title}</p>;
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        As a rising junior in computer science, I'm skilled in languages like
+        JavaScript, TypeScript, Java, C++, Python, and frameworks such as React,
+        Node.js, Express, and Spring. I excel in challenging environments, adept
+        at problem-solving and developing efficient, innovative software. I'm a
+        team player with a drive for continuous learning, dedicated to pushing
+        boundaries and driving growth with top-tier software solutions.{" "}
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
